243905ea1c8b1179cab5f01f062c51c234cfe723
isaacbarcroft <i_barcroft@yahoo.com>
2021-10-13 15:42:26 -0400

    views

--- chats/views.py
+++ chats/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Room, Message
 from .serializers import RoomSerializer, MessageSerializer
 from rest_framework import generics 
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
@@ -19,6 +20,10 @@ class MessageListAPIView(generics.ListCreateAPIView):
     def get_queryset(self):
         room = self.kwargs['room']
         return Message.objects.filter(room=room)
+
+    def perform_create(self,serializer):
+        room = get_object_or_404(Room, id=self.kwargs['room'])
+        serializer.save(room=room)
 
 class MessageDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Message.objects.all()
--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -1,36 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-
+  const [message, setMessage] = useState('')
 
   async function submitMessage(name, text){
-    const newOrder = {
+    const newMessage = {
       user: name, 
       body: text, 
-     
-     
+    
     };
-    console.log(newOrder);
-    const response = await fetch('https://django-restaurant-app--isaac.herokuapp.com/api_v1/orders/', {
+    console.log();
+    const response = await fetch('https://django-rest-chat-app-isaac.herokuapp.com/api_v1/chats/{}/message', {
       method: 'POST',
       headers: {
         'Content-Type' : 'application/json',
       },
-      body: JSON.stringify(newOrder),
+      body: JSON.stringify(newMessage),
     });
     if(response.ok){
-      return response.json();
-    
+      return response.json(); 
 }  
   }
+  function addMessage(title, price){
 
+    const newMessageText = {
+      , 
+      price,
+    }
+    setOrder([...order, newOrderItem]);
+  }
 
 
 
   return (
     <div className="App">
-     
+     <MessageForm 
+
     </div>
   );
 }
