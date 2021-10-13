7c029bd33b53f626b51c00f82a9b6796f5324d87
isaacbarcroft <i_barcroft@yahoo.com>
2021-10-13 16:00:33 -0400

    urls

--- chats/urls.py
+++ chats/urls.py
@@ -5,8 +5,8 @@ app_name = 'chats'
 
 
 urlpatterns = [ 
-    path('<int:chat>/message/<int:pk>/', views.MessageDetailAPIView.as_view(), name="message_detail"),
-    path('<int:chat>/message/', views.MessageListAPIView.as_view(), name="message_list"),
+    path('<int:room>/message/<int:pk>/', views.MessageDetailAPIView.as_view(), name="message_detail"),
+    path('<int:room>/message/', views.MessageListAPIView.as_view(), name="message_list"),
     path('<int:pk>/', views.RoomDetailAPIView.as_view(), name="room_detail"),
     path('', views.RoomListAPIView.as_view(), name='room_list'),
 ]
